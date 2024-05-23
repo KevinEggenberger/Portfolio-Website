d09bef2dd8c46f5a80b37ba34a3576b3a9cc9ff6
KevinEggenberger <162747184+KevinEggenberger@users.noreply.github.com>
2024-05-23 23:44:08 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+// js/script.js
+import './top_button.js';
+// Füge hier weitere JavaScript-Dateien hinzu, falls nötig
+
+// Du kannst hier auch allgemeine Funktionen oder Initialisierungen vornehmen
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('All scripts loaded');
+});
